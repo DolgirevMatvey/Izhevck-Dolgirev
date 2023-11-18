16cce969a617709574ffae13160d52bfdd6e432d
Yegor Knyazev <egorknyazev10@gmail.com>
2023-11-18 19:14:02 +0400

    second_zadach

--- main.py
+++ main.py
@@ -1,1 +1,38 @@
-print("My first git-repo")
+import sys
+import random
+
+from PyQt5.QtGui import QPainter, QColor
+from PyQt5.QtWidgets import QWidget, QApplication, QPushButton, QMainWindow
+from PyQt5 import uic
+
+
+class Example(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('UI.ui', self)
+        self.do_paint = False
+        self.pushButton.clicked.connect(self.paint)
+
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw_circle(qp)
+            qp.end()
+        self.do_paint = False
+
+    def paint(self):
+        self.do_paint = True
+        self.update()
+
+    def draw_circle(self, qp):
+        qp.setBrush(QColor(255, 255, 0))
+        x = random.randrange(10, 50)
+        qp.drawEllipse(50, 50, x, x)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    ex.show()
+    sys.exit(app.exec())
